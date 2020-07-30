3eb850a471c4ac58530bf4bd97fc249d3c8df42b
stapler34 <stapler741@gmail.com>
2020-07-30 23:51:04 +0800

    Detects the coordinate of the mouse after 5 sec

---
+++ coordinate.py
@@ -0,0 +1,11 @@
+import pyautogui #use to simulate mouse and keyboard movement
+from time import sleep
+pyautogui.PAUSE = 1 #pause for 1 sec before executing pyautogui function call
+sleep(5) #wait for 5 sec
+
+x,y = pyautogui.position() #inputs the current mouse position after 5 sec
+print(x,y) #prints out the mouse position
+
+pyautogui.moveTo(x,y) #simulate mouse movement to the coordinates
+pyautogui.doubleclick() #double click on the coordinates
+
