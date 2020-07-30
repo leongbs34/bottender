e9a8451851cc11b11495c4dbceb661e9cc7bd325
stapler34 <stapler741@gmail.com>
2020-07-31 04:33:17 +0800

    Opens google chrome and enables user to select google meet class link

--- bottender.py
+++ bottender.py
@@ -1,31 +1,32 @@
 import pyautogui as pg
 import re
+import os
 
 def getLink(): #get the line of selected google meet link
     try:
-        linkSelect = input('Please enter the digit of your selected google meet link; eg: 1,2,3,...')
-        
+        linkSelect = int(input('Please enter the digit of your selected google meet link; eg: 1,2,3,...\n'))
     except ValueError:
         print('Error, please enter only digits')
         getLink()
-    return int(linkSelect)
+    return linkSelect
 
 
 pg.hotkey('winleft') #press windows key on keyboard
 
-pg.typewrite('chrome\n') #type in chrome in search bar and enter
+pg.typewrite('chrome\n') #type in chrome in search bar and enterchrome
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+meetlink = os.path.join(THIS_FOLDER, 'meetlink.txt')
 
-fp = open('meetlink.txt', r+)#line to read google meet link
-for lines in fp.read().split('\n'): #output selection of meet link
-    i = 1
-    print(i + ': ' + lines+1)
-    i += 1
-linkRegex = re.compile(r'https.*')
-mo = linkRegex.findall(fp.read())
+#fp = open(meetlink, 'r') #line to read google meet link
+i = 0
+with open(meetlink, 'r') as fp:
+    for lines in (fp.read().split('\n'))[1:]: #output selection of meet link
+        i += 1
+        print(str(i) + ': ' + lines)
+
+with open(meetlink, 'r') as fp:
+    mo = re.findall('https.*', fp.read())
 link = mo[getLink()-1] #store google meet link to 'link' variable
-print(link)
-
-
 
 
 #pg.typewrite(gmeet) #enter link from meetlink.txt
