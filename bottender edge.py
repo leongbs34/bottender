9765f3bb34d56912531e7bc9d4af47a15b69d94a
unknown <stapler_1@hotmail.com>
2020-08-03 02:50:36 +0800

    Updated edge version

--- bottender edge.py
+++ bottender edge.py
@@ -1,7 +1,7 @@
 import pyautogui as pg
 import sys
-from desktopmagic.screengrab_win32 import getScreenAsImage 
 import re
+from desktopmagic.screengrab_win32 import getScreenAsImage 
 import os
 import glob
 from time import time
@@ -25,7 +25,7 @@ start_time = time() #start timer
 
 i = 0
 with open(meetlink, 'r') as fp:
-    for lines in (fp.read().split('\n'))[2:]: #output selection of meet link
+    for lines in (fp.read().split('\n'))[1:]: #output selection of meet link
         i += 1
         print(str(i) + ': ' + lines)
 
@@ -33,31 +33,44 @@ with open(meetlink, 'r') as fp:
     mo = re.findall('https.*', fp.read())
 link = mo[getLink()-1] #store google meet link to 'link' variable
 
+print('''Bot will take control of your computer for awhile, please do not press anything until mention
+Beginning in 3 seconds...''')
+sleep(3)
+
+print('Bot started')
 pg.hotkey('winleft') #press windows key on keyboard
 sleep(1)
 pg.typewrite('edge\n',0.1) #type in edge in search bar and enter
 sleep(5)
+pg.click(662,53) #click on url 
 pg.typewrite(link + '\n') #enter link from meetlink.txt
-pg.hotkey('winkey','up') #fullscreen edge
+pg.hotkey('winkey','up') #fullscreen google edge
 sleep(5)
 pg.hotkey('ctrl','d') #mute mic
 sleep(1)
 pg.hotkey('ctrl','e') #hide cam
 
-pg.click(-376,733) #join classroom
+sleep(3)
+join = pg.locateOnScreen('join.png', confidence=0.9)
+pg.click(join[0], join[1]) #join classroom
 sleep(1)
-pg.click(-253,382) #show chat
+chat = pg.locateOnScreen('chat.png', confidence=0.9)
+pg.click(chat[0], chat[1]) #show chat
 
 #open obs
 pg.hotkey('winleft') #press windows key on keyboard
 sleep(1)
 pg.typewrite('obs\n',0.1)
-sleep(4)
+sleep(5)
 
 #start recording , requires start recording and stop recording hotkey
-pg.hotkey('f1')
+hotkeyRec = 'f1'
+pg.hotkey(hotkeyRec)
 sleep(1)
 pg.hotkey('alt','\t') #alt tab into edge
+
+print('''Scanning for QR code...
+Bot control paused until QR code is found on screen''')
 
 #QR scanner
 while True:
@@ -83,15 +96,16 @@ while True:
                 hours = (time() - start_time ) / 3600
                 if attendance != '': #if attendance found break loop
                         print('Attendance link found: ' + attendance)
-                        sleep(2)
+                        print('Bot control beginning in 3 seconds, please do not touch anything until you are told')
+                        sleep(3)
                         break
 
                 if hours >= 2:
                     print('Attendance link not found after 2h')
                     sleep(1)
-                    pg.hotkey('ctrl','w') #close chrome
+                    pg.hotkey('ctrl','w') #close edge
                     sleep(1)
-                    pg.hotkey('f1') #stop recording
+                    pg.hotkey(hotkeyRec) #stop recording
                     sys.exit()
                     
         except:
@@ -103,9 +117,10 @@ pg.hotkey('ctrl','t') #open new tab
 sleep(1)
 pg.typewrite(attendance+'\n') #go to attendance link
 sleep(3)
-pg.click(-923,657) #sign attendance
+pg.press('enter') #sign attendance
 sleep(5)
 pg.hotkey('ctrl','w') #close attendance tab
+print('Attendance signed, you may use your computer now, recording will stop after 2 hours')
 
 #after 2 hours, stop recording, close edge and program
 while True:
@@ -115,5 +130,5 @@ while True:
         sleep(1)
         pg.hotkey('ctrl','w') #close edge
         sleep(1)
-        pg.hotkey('f1') #stop recording
+        pg.hotkey(hotkeyRec) #stop recording
         sys.exit()
